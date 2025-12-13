c2e56d7e68d364c1760e647af258303478847037
LuisCarlosPicadoRojas <144180800+LuisCarlosP@users.noreply.github.com>
2025-12-13 00:07:38 -0600

    Terminal v1

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 import { LanguageProvider } from './providers/LanguageProvider'
@@ -10,9 +10,9 @@ import '@fortawesome/fontawesome-free/css/all.min.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <LanguageProvider>
-            <BrowserRouter basename={import.meta.env.BASE_URL}>
+            <HashRouter>
                 <App />
-            </BrowserRouter>
+            </HashRouter>
         </LanguageProvider>
     </React.StrictMode>,
 )
